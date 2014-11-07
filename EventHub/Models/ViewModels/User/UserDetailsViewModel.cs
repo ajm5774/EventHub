44ac9f164ac301fd631369c6ee55eb8bcf6f396e
akelley0 <ajk6045@rit.edu>
2014-11-06 23:56:50 -0500

    added user details page and view model

---
+++ EventHub/Models/ViewModels/User/UserDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventHub.Models.ViewModels.User
+{
+    public class UserDetailsViewModel
+    {
+        public string UserId{ set; get; }
+        public string UserName { set; get; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public string SchoolName { set; get; }
+
+    }
+}
