1d489a740611f6766ad822c6bb9817f83ae931db
akelley0 <ajk6045@rit.edu>
2014-11-09 16:12:02 -0500

    Added user info below user pic and added create group button and modal above group list. also, when creating a group, creating user is automatically subscribed to that group

--- EventHub/Models/ViewModels/Home/IndexViewModel.cs
+++ EventHub/Models/ViewModels/Home/IndexViewModel.cs
@@ -8,5 +8,9 @@ namespace EventHub.Models
     public class IndexViewModel
     {
         public string PicturePath { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string UserId { get; set; }
     }
 }
